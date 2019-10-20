e0f3468976fe517aa3d91c0b54569a00713b15d5
Diyar Faraj <48604250+diyarfaraj@users.noreply.github.com>
2019-10-20 14:55:55 +0200

    dynimc data showing in blog pages

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,38 +1,43 @@
-import React from 'react';
-import Layout from '../components/layout';
-import { graphql, useStaticQuery } from 'gatsby';
+import React from "react"
+import Layout from "../components/layout"
+import { Link, graphql, useStaticQuery } from "gatsby"
 
 const BlogPage = () => {
-	const BlogData = useStaticQuery(graphql`
-		query {
-			allMarkdownRemark {
-				edges {
-					node {
-						frontmatter {
-							title
-							date
-						}
-					}
-				}
-			}
-		}
-	`);
+  const BlogData = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
 
-	return (
-		<Layout>
-			<h1>Vår Blog</h1>
-			<ol>
-				{BlogData.allMarkdownRemark.edges.map((edge) => {
-					return (
-						<li>
-							<h2>{edge.node.frontmatter.title}</h2>
-							<p>{edge.node.frontmatter.date}</p>
-						</li>
-					);
-				})}
-			</ol>
-		</Layout>
-	);
-};
+  return (
+    <Layout>
+      <h1>Vår Blog</h1>
+      <ol>
+        {BlogData.allMarkdownRemark.edges.map(edge => {
+          return (
+            <li>
+              <Link to={`/blog/${edge.node.fields.slug}`}>
+                <h2>{edge.node.frontmatter.title}</h2>
+                <p>{edge.node.frontmatter.date}</p>
+              </Link>
+            </li>
+          )
+        })}
+      </ol>
+    </Layout>
+  )
+}
 
-export default BlogPage;
+export default BlogPage
