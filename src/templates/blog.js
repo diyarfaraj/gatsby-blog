8993cb934794984d7a237465df858fce28640f62
Diyar Faraj <diyar.faraj@gmail.com>
2019-10-21 17:18:26 +0200

    contenful woring fine

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Layout from '../components/layout';
 import { graphql } from 'gatsby';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 export const query = graphql`
 	query($slug: String!) {
 		contentfulBlogPost(slug: { eq: $slug }) {
 			title
 			publishedDate(formatString: " MMMM Do, YYYY")
+			body {
+				json
+			}
 		}
 	}
 `;
@@ -16,6 +20,7 @@ const Blog = (props) => {
 		<Layout>
 			<h1>{props.data.contentfulBlogPost.title}</h1>
 			<p>{props.data.contentfulBlogPost.publishedDate}</p>
+			{documentToReactComponents(props.data.contentfulBlogPost.body.json)}
 		</Layout>
 	);
 };
