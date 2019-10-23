4fcfeace8cfbee6e95c58b2f07e7f0053b0d22c9
Diyar Faraj <diyar.faraj@gmail.com>
2019-10-23 06:26:32 +0200

    swedish title

--- src/components/header.js
+++ src/components/header.js
@@ -1,69 +1,57 @@
-import React from "react"
-import { Link, graphql, useStaticQuery } from "gatsby"
-import headerStyles from "./header.module.scss"
+import React from 'react';
+import { Link, graphql, useStaticQuery } from 'gatsby';
+import headerStyles from './header.module.scss';
 
 //Display dynamic info
 const Header = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+	const data = useStaticQuery(graphql`
+		query {
+			site {
+				siteMetadata {
+					title
+				}
+			}
+		}
+	`);
 
-  //Header nav
-  return (
-    <header className={headerStyles.header}>
-      <h1>
-        <Link className={headerStyles.title} to="/">
-          {data.site.siteMetadata.title}
-        </Link>
-      </h1>
-      <nav>
-        <ul className={headerStyles.navList}>
-          <li>
-            <Link
-              className={headerStyles.navItem}
-              activeClassName={headerStyles.activeNavItem}
-              to="/"
-            >
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              className={headerStyles.navItem}
-              activeClassName={headerStyles.activeNavItem}
-              to="/contact"
-            >
-              Contact
-            </Link>{" "}
-          </li>
-          <li>
-            <Link
-              className={headerStyles.navItem}
-              activeClassName={headerStyles.activeNavItem}
-              to="/about"
-            >
-              About
-            </Link>{" "}
-          </li>
-          <li>
-            <Link
-              className={headerStyles.navItem}
-              activeClassName={headerStyles.activeNavItem}
-              to="/blog"
-            >
-              Blog
-            </Link>{" "}
-          </li>
-        </ul>
-      </nav>
-    </header>
-  )
-}
+	//Header nav
+	return (
+		<header className={headerStyles.header}>
+			<h1>
+				<Link className={headerStyles.title} to="/">
+					{data.site.siteMetadata.title}
+				</Link>
+			</h1>
+			<nav>
+				<ul className={headerStyles.navList}>
+					<li>
+						<Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/">
+							Hem
+						</Link>
+					</li>
+					<li>
+						<Link
+							className={headerStyles.navItem}
+							activeClassName={headerStyles.activeNavItem}
+							to="/contact"
+						>
+							Kontakt
+						</Link>{' '}
+					</li>
+					<li>
+						<Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/about">
+							Om Oss
+						</Link>{' '}
+					</li>
+					<li>
+						<Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/blog">
+							Blogg
+						</Link>{' '}
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
+};
 
-export default Header
+export default Header;
